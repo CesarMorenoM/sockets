ebdb1021526d0b8760532b75c5b7fbc1f64c7453
CesarMoreno <alumcesarmoreno1@gmail.com>
2020-12-11 14:31:26 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const SockerIO = require('socket.io');
+
+// settings 
+app.set('port', process.env.PORT || 3000);
+
+//static files
+app.use(express.static(path.join(__dirname,'public')));
+
+
+const server = app.listen(app.get('port'),()=>{
+    console.log('server on port', app.get('port'));
+});
+
+const io = SockerIO(server);
+
+
+//websockerts
+io.on('connection', (socket) =>{
+    console.log('New connection bro ',socket.id);
+
+    socket.on('chat:message', (data) => {
+        io.sockets.emit('chat:message', data);
+    })
+
+    socket.on('chat:typing', (data)=>{
+        socket.broadcast.emit('chat:typing', data);
+    })
+});
+
